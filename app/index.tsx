ce07efbb3aafa58f6dc0ed5754515afc1bb7d98f
UP240575 <up240575@alumnos.upa.edu.mx>
2026-03-02 18:11:46 -0600

    add getPokemon

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,24 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    console.log("Entre en pantalla");
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+    const response = await fetch(URL, {
+      method: "GET",
+    });
+
+    const data = await response.json();
+    console.log(response);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
