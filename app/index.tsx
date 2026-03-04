cfa5d4d1f0b5af1b50909ad76ed48d2bf49239af
UP240575 <up240575@alumnos.upa.edu.mx>
2026-03-04 17:42:49 -0600

    Show pokemons on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,28 @@
-import { useEffect } from "react";
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text } from "react-native";
 
 export default function Index() {
+  const [results, setResults] = useState<any[]>([]);
   useEffect(() => {
     console.log("Entre en pantalla");
     getPokemons();
   }, []);
 
   const getPokemons = async () => {
-    const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
     const response = await fetch(URL, {
       method: "GET",
     });
-
     const data = await response.json();
-    setResults(data.response);
+    setResults(data.results);
     console.log(data);
   };
 
   return (
-    <View>
+    <ScrollView>
       {results.map((pokemon) => (
         <Text key={pokemon.name}>{pokemon.name}</Text>
       ))}
-    </View>
+    </ScrollView>
   );
 }
